81131f4e31812b7653f4f7945898a085802d13b6
artem-tkachuk <artemtkachuk@yahoo.com>
2020-03-07 19:29:01 -0800

    change to structure. Integrated the homepage

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,3 +1,3 @@
 exports.getHome = (req, res) => {
-    res.send(`Hello, world!`);
-}
+    res.render(`user/index`);
+};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const app = express();
 const getHomeRoutes = require('./routes/home');
 const get404 = require('./controllers/404').get404;
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
+
 app.use(getHomeRoutes);
 app.use(get404);
 
